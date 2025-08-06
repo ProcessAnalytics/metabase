488264bd334754e5f4398215010051b6e9103ffe
atatulyan <a.tatulyan@endrex.net>
2025-08-06 18:58:06 +0300

    add router

--- src/metabase/api/routes.clj
+++ src/metabase/api/routes.clj
@@ -82,7 +82,7 @@
   (context "/geojson"              [] api.geojson/routes)
   (context "/google"               [] (+auth api.google/routes))
   (context "/ldap"                 [] (+auth api.ldap/routes))
-  (context "/openid"               [] (+auth api.openid/routes))
+  (context "/openid"               [] api.openid/routes)
   (context "/login-history"        [] (+auth api.login-history/routes))
   (context "/premium-features"     [] (+auth api.premium-features/routes))
   (context "/metric"               [] (+auth api.metric/routes))
