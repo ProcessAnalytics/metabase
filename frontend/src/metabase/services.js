70e697944d506d19c492990d2d729c2458b0a502
a.tatulyan <a.tatulyan@tinkoff.ru>
2025-08-13 22:46:03 +0300

    checkpoint

--- frontend/src/metabase/services.js
+++ frontend/src/metabase/services.js
@@ -361,7 +361,7 @@ export const RevisionsApi = {
 export const SessionApi = {
   create: POST("/api/session"),
   createWithGoogleAuth: POST("/api/session/google_auth"),
-  createWithTWorkAuth: POST("/api/session/twork_auth"),
+  createWithTWorkAuth: POST("/api/session/twork/auth"),
   delete: DELETE("/api/session"),
   properties: GET("/api/session/properties"),
   forgot_password: POST("/api/session/forgot_password"),
